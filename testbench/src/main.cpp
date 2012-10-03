d9e0a2fc28b1340153d5e06be1f09dbbea982897
trannz <trannz@trannztop.(none)>
2012-10-03 18:21:31 +0200

    Trwa rozwój klasy PGpoPin służącej do obsługi jednego pinu wyjściowego

--- testbench/src/main.cpp
+++ testbench/src/main.cpp
@@ -16,6 +16,69 @@
 #include "hal/preg8.h"
 //#include "hal/preg16.h"
 //#include "hal/gpio.h"
+#include "hal/onoff.h"
+
+
+class PGpoPin
+{
+public:
+    PGpoPin(volatile uint8_t &port, const uint8_t pin, bool offState);
+    void set(bool value);
+    bool get();
+    void on();
+    void off();
+    void toggle();
+    void highZ(const bool enable);
+
+private:
+    volatile uint8_t &PORT;
+    volatile uint8_t &DDR;
+    uint8_t PINmask;
+    bool os;
+
+
+};
+
+PGpoPin::PGpoPin(volatile uint8_t &port, const uint8_t pin, bool offState):PORT(port),DDR(*(&port-1)),PINmask(1<<pin),os(offState)
+{
+    highZ(false);
+}
+
+inline void PGpoPin::set(bool value)
+{
+    value^=os;
+    PReg8::set(PORT,0xff*value,PINmask);
+}
+
+inline bool PGpoPin::get()
+{
+    return ((bool)(PORT&PINmask))^os;
+}
+
+inline void PGpoPin::on()
+{
+    set(true);
+}
+
+inline void PGpoPin::off()
+{
+    set(false);
+}
+
+inline void PGpoPin::toggle()
+{
+    PORT^=PINmask;
+}
+
+void PGpoPin::highZ(const bool enable)
+{
+    if(enable)
+        DDR&=~(PINmask);
+    else
+        DDR|=(PINmask);
+}
+
+
 
 
 int main(void)
@@ -27,7 +90,7 @@ int main(void)
     volatile uint8_t fakerejestr=0b01010101;
     //volatile uint16_t fake2=0b0101010101010101;
 
-    PReg8 MyReg(fakerejestr);
+    //PReg8 MyReg(fakerejestr);
     //My8Preg MyRejestr(fakerejestr);
     //PReg8<uint16_t> MyRej2(fake2);
 
@@ -36,14 +99,20 @@ int main(void)
     //PReg8::set(fakerejestr,0b00110011,0b00001111);
     //PReg16::set(fake2,0b0011001100110011,0b0000111100001111);
 
-    uint8_t a=0xff;
 
+    PGpoPin led1(PORTD,4,0);
 
+    led1.off();
+    led1.highZ(true);
+
+    uint8_t a=0;
+
+    a=DDRD;
     //uint8_t v=0;
     //A a(v);
     //a=PReg8::get(fakerejestr,a,0x0f);
-    MyReg.set(0xff,0x0f);
-    a=fakerejestr;
+    //MyReg.set(0xff,0x0f);
+    //a=((bool)(fakerejestr&(1<<5)))^false;
     //uint16_t a=fake2;
 
 
